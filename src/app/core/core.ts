e4ee57a8ef978b464dfc5649d41338993cae464e
Huynh Hoang Huy <huynhhuy.forwork@gmail.com>
2026-05-07 16:20:17 +0700

    chore: rename dir

--- src/app/core/core.ts
+++ src/app/core/core.ts
@@ -1,6 +1,6 @@
 import { provideRouter, Routes, TitleStrategy } from '@angular/router'
 import { provideBrowserGlobalErrorListeners } from '@angular/core'
-import { provideZard } from './provider/provide-zard'
+import { provideZard } from './providers/provide-zard'
 import { PageTitleStrategy } from '@ph/core/configs/page-title-strategy'
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'
 import { apiInterceptor } from '@ph/core/interceptors/api-interceptor'
